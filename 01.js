838b30b75f203a4408ee9b5c2f908a2cb8235998
Matheus Marques <146294794+math-marques@users.noreply.github.com>
2025-03-20 19:20:49 -0300

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,4 @@
+// exercicio do mário e do listao de logica
+console.log("João é lindíssimo!");
+//typerscript não roda no navegador. por conta disso, ele faz uma TRANSPILAÇÃO, transformando em JS para jogar no navegador. 
+// Por isso que rodei no cmd = tsc 01.ts
