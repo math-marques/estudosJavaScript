2357ee51377e10017b37f8cf9f90dcecf7fd7ba0
Matheus Marques <146294794+math-marques@users.noreply.github.com>
2025-09-02 12:11:07 -0300

    Update 13.js

--- estudosJavaScript-main/1-EstruturaSequencial/13.js
+++ estudosJavaScript-main/1-EstruturaSequencial/13.js
@@ -11,5 +11,5 @@ const altura = parseFloat(prompt("sua altura em metros:"));
 const pesoIdealHomen = IMCH(alturaHomem);
 const pesoIdealMulher = IMCM(alturaMulher);
 
-console.log(`Primeiro caso (Homens) = Com base na altura de ${alturaHomem}m, o peso ideal é ${pesoIdealHomen.toFixed(2)}kg.`);
+console.log(`Primeiro caso (Homens) = Com base na altura de ${alturaHomem}m, o peso ideal é ${pesoIdealHomen.toFixed(2)}kg.`); //interpolação
 console.log(`Segundo caso (Mulheres) = Com base na altura de ${alturaMulher}m, o peso ideal é ${pesoIdealMulher.toFixed(2)}kg.`);
